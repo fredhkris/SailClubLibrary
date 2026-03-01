1268ce23e0c439586a0a322c1f2a2896288fab98
Frederik Kristensen <frede.h.k1@gmail.com>
2026-03-01 13:44:48 +0100

    wip booking

--- RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
+++ RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SailClubLibrary.Interfaces;
+using SailClubLibrary.Models;
 
 namespace RazorBoatApp2026.Pages.Bookings
 {
     public class CreateBookingModel : PageModel
     {
-        public void OnGet()
+        private IBoatRepository _repo;
+        public Boat Boat { get; set; }
+
+        public CreateBookingModel(IBoatRepository repo)
         {
+            _repo = repo;
+        }
+
+        public void OnGet(string sailNumber)
+        {
+            Boat = _repo.SearchBoat(sailNumber);
+        }
+
+        public IActionResult OnPost(string sailNumber)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- RazorBoatApp2026/Program.cs
+++ RazorBoatApp2026/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IBoatRepository, BoatRepository>();
 builder.Services.AddSingleton<IMemberRepository, MemberRepository>();
+builder.Services.AddSingleton<IBookingRepository, BookingRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
