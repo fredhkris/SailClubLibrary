278b547564601ec2c79f1ee89edb9b55e8afc770
fredhkris <frede.h.k1@gmail.com>
2026-03-02 22:59:16 +0100

    wip

--- RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
+++ RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
@@ -2,13 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SailClubLibrary.Interfaces;
 using SailClubLibrary.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorBoatApp2026.Pages.Bookings
 {
     public class CreateBookingModel : PageModel
     {
         private IBoatRepository _repo;
+
         public Boat Boat { get; set; }
+
+        [BindProperty]
+        [MinLength(8), MaxLength(8)]
+        public string PhoneNumber { get; set; }
+
+        [BindProperty]
+        public DateTime StartDate { get; set; }
+
+        [BindProperty]
+        public DateTime EndDate { get; set; }
 
         public CreateBookingModel(IBoatRepository repo)
         {
@@ -22,7 +34,12 @@ namespace RazorBoatApp2026.Pages.Bookings
 
         public IActionResult OnPost(string sailNumber)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _repo.AddBoat(Boat);
+            return Page();
         }
     }
 }
