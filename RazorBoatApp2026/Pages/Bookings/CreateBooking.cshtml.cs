a149368fe46e9cae36cba4b05228687aa5aff9e0
fredhkris <frede.h.k1@gmail.com>
2026-03-03 23:07:09 +0100

    More Booking...

--- RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
+++ RazorBoatApp2026/Pages/Bookings/CreateBooking.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SailClubLibrary.Exceptions;
 using SailClubLibrary.Interfaces;
 using SailClubLibrary.Models;
 using System.ComponentModel.DataAnnotations;
@@ -8,28 +9,35 @@ namespace RazorBoatApp2026.Pages.Bookings
 {
     public class CreateBookingModel : PageModel
     {
-        private IBoatRepository _repo;
+        private IBoatRepository _boatRepo;
+        private IBookingRepository _bookingRepo;
 
-        public Boat Boat { get; set; }
+        [BindProperty]
+        public Boat? Boat { get; set; }
 
         [BindProperty]
         [MinLength(8), MaxLength(8)]
+        [Required(AllowEmptyStrings = false)]
+        //[RegularExpression(@"^[0-9]*$", ErrorMessage = "Must be a number")]
         public string PhoneNumber { get; set; }
 
         [BindProperty]
-        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
 
         [BindProperty]
-        public DateTime EndDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; } = DateTime.Now;
 
-        public CreateBookingModel(IBoatRepository repo)
+        public CreateBookingModel(IBoatRepository boatRepo, IBookingRepository bookingRepo)
         {
-            _repo = repo;
+            _boatRepo = boatRepo;
+            _bookingRepo = bookingRepo;
         }
 
         public void OnGet(string sailNumber)
         {
-            Boat = _repo.SearchBoat(sailNumber);
+            Boat = _boatRepo.SearchBoat(sailNumber);
         }
 
         public IActionResult OnPost(string sailNumber)
@@ -38,8 +46,22 @@ namespace RazorBoatApp2026.Pages.Bookings
             {
                 return Page();
             }
-            _repo.AddBoat(Boat);
-            return Page();
+            Boat = _boatRepo.SearchBoat(sailNumber);
+            if (Boat == null)
+            {
+                return RedirectToPage("Index");
+            }
+            try
+            {
+                Member member = new Member(0, "Hans", "Hansen", PhoneNumber, "Vej 123", "By", "mail@mail.dk", MemberType.Adult, MemberRole.Member);
+                _bookingRepo.AddBooking(new Booking(0, StartDate, EndDate, "", member, Boat));
+            }
+            catch (Exception e)
+            {
+                ViewData["ErrorMessage"] = e.Message;
+                return Page();
+            }
+            return RedirectToPage("Index");
         }
     }
 }
