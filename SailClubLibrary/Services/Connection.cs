b322d5012e609ad489515f7336934daf8cc2d690
Frederik Kristensen <frede.h.k1@gmail.com>
2026-04-21 17:24:06 +0200

    Commit crude async prototype

---
+++ SailClubLibrary/Services/Connection.cs
@@ -0,0 +1,17 @@
+﻿namespace SailClubLibrary.Services
+{
+    // Opbevares lokalt
+    public class Connection
+    {
+        public static readonly string connectionString =
+            "Data Source=(localdb)\\MSSQLLocalDB;" +
+            "Initial Catalog=SailClubDatabase;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;" +
+            "Encrypt=True;" +
+            "Trust Server Certificate=False;" +
+            "Application Intent=ReadWrite;" +
+            "Multi Subnet Failover=False;" +
+            "Command Timeout=30";
+    }
+}
